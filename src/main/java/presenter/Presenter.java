ca5b6a3c18e7003d52648215d427c15972e5a760
Evgegeni <popovevgeni81@gmail.com>
2023-02-24 15:57:32 +0800

    version_4

--- src/main/java/presenter/Presenter.java
+++ src/main/java/presenter/Presenter.java
@@ -4,7 +4,9 @@ import model.Animals;
 import model.Reestr;
 import view.View;
 
+import java.lang.reflect.Method;
 import java.text.ParseException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import java.util.ArrayList;
@@ -23,12 +25,12 @@ public class Presenter {
         boolean flag = true;
         while (flag) {
             System.out.println();
-            System.out.println("Выберите действие: 1 - посмотреть всех животных, 2 - создать новое животное, 0 - выход");
+            System.out.println("Выберите действие: 1 - посмотреть всех животных, 2 - создать новое животное, 3 - посмотреть, что умеет животное 0 - выход");
             String key = iScanner.next();
             switch (key) {
                 case "2":
                     reestr.addAnimals(view.setAnimals());
-//                    System.out.println(reestr);
+                    System.out.println("Вы создали: ");
                     break;
                 case "1":
                     if (reestr.getReestr() == null) {
@@ -37,6 +39,23 @@ public class Presenter {
                     } else {
                         for (Animals animal : reestr.getReestr().values()) {
                             System.out.println(reestr);
+                        }
+                    }
+                    break;
+                case "3":
+                    System.out.println("Выбирите животное, введя его номер: ");
+                    for (Animals animal : reestr.getReestr().values()) {
+                        System.out.println(reestr);
+                    }
+                    int keyId = iScanner.nextInt();
+
+                    Method[] methodsObject = Object.class.getMethods();
+                    Method[] methods = reestr.getReestr().get(keyId).getClass().getMethods();
+                    String[] actionS = {"getName", "toString", "setName", "setBirthday", "getBirthday"};
+                    for (Method m : methods) {
+                        if (!Arrays.asList(methodsObject).contains(m) &&
+                                (!Arrays.asList(actionS).contains(m.getName()))) {
+                            System.out.println(m.getName());
                         }
                     }
                     break;
